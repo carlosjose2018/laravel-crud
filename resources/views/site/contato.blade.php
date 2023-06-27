ac0137f5350b48ae99338594ee19186c65376ebd
carlosjose2018 <carlosaraujo10jose3000@gmail.com>
2023-06-27 13:01:44 -0300

    <FEAT>:implemente components and pages contato e principal.

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -12,27 +12,10 @@
 
             <div class="informacao-pagina">
                 <div class="contato-principal">
-                    <form action={{route('site.contato')}} method="POST">
-                        
-                        @csrf
 
-                        <input name='name' type="text" placeholder="Nome" class="borda-preta">
-                        <br>
-                        <input name="phone" placeholder="Telefone" class="borda-preta">
-                        <br>
-                        <input type="email" placeholder="E-mail" class="borda-preta">
-                        <br>
-                        <select class="borda-preta">
-                            <option value="">Qual o motivo do contato?</option>
-                            <option value="1">Dúvida</option>
-                            <option value="2">Elogio</option>
-                            <option value="3">Reclamação</option>
-                        </select>
-                        <br>
-                        <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
-                        <br>
-                        <button type="submit" class="borda-preta">ENVIAR</button>
-                    </form>
+                    @component('site.components.form_layout')    
+                    @endcomponent
+
                 </div>
             </div>  
         </div>
